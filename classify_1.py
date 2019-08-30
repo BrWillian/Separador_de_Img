7893f556603f35dee7a976e74a520012b39805cf
Willian Moreira Antunes <willianmoreira@vizentec.com.br>
2019-08-30 15:49:46 -0300

    Separador de imagem

--- classify_1.py
+++ classify_1.py
@@ -1,14 +1,10 @@
 from os import walk,path,getcwd
-import cv2
+from cv2 import imread,cvtColor,threshold
 from shutil import move
-from tempo import tempo
 from numpy import mean,median,sum
-
-tipo_imagem = ['.jpg']
 
 index = [96,160,187]
 
-@tempo
 def func_filter_dir(directory):
     try:
         for (dirpath, dirnames, filenames) in walk(directory):
@@ -24,13 +20,10 @@ def func_filter_dir(directory):
 
 
 def filter(directory):
-    img = cv2.imread(directory)
-    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    equ_img = cv2.equalizeHist(img_gray)
-    img_bin = cv2.threshold(img_gray, 40, 255, cv2.THRESH_BINARY)[1]
+    img = imread(directory)
+    img_gray = cvtColor(img, cv2.COLOR_RGB2GRAY)
+    equ_img = equalizeHist(img_gray)
+    img_bin = threshold(img_gray, 40, 255, cv2.THRESH_BINARY)[1]
     result = sum(mean(img_bin)+mean(equ_img)) / 2.0
 
     return result
-
-if __name__ == "__main__":
-    func_filter_dir('C:/Users/willianmoreira/Desktop/vuc')
