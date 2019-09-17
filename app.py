979aa88b089a393a321274a27b9cdb3f281a7dca
brwillian <wiliam-m-@hotmail.com>
2019-09-17 17:32:31 -0300

    Separador de imagens

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
-import os
 import classify_1
 import classify_2
 import classify_3
 import classify_4
 import list_dir
+import sys
+import os
+from design import *
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QProgressBar
 
-directory = input('Digite o caminho do diretorio: ')
-directory = directory.replace('\\','/')
-list_dir.list_dir(directory)
-classify_1.func_filter_dir(directory)
-classify_2.func_filter_dir(directory)
-classify_3.func_filter_dir(directory)
-classify_4.func_filter_dir(directory)
+class Separador(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        super().setupUi(self)
+        self.caminho = None
+        self.pushButton_2.clicked.connect(self.abrir_imagem)
+        self.pushButton.clicked.connect(self.processar)
 
-os.removedirs(directory+'/cache/')
+    def abrir_imagem(self):
+        self.caminho = QFileDialog.getExistingDirectory(
+            self.centralwidget,
+            'Selecione caminho das imagens',
+            rf'{os.getcwd()}'
+        )
+        self.lineEdit.setText(self.caminho)
+
+    def processar(self):
+        list_dir.list_dir(self.caminho)
+        classify_1.func_filter_dir(self.caminho)
+        self.progressBar.setValue(25)
+        classify_2.func_filter_dir(self.caminho)
+        self.progressBar.setValue(50)
+        classify_3.func_filter_dir(self.caminho)
+        self.progressBar.setValue(75)
+        classify_4.func_filter_dir(self.caminho)
+        self.progressBar.setValue(100)
+        os.removedirs(self.caminho + '/cache/')
+
+if __name__ == '__main__':
+    qt = QApplication(sys.argv)
+    separador = Separador()
+    separador.show()
+    qt.exec_()
---
+++ design.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'design.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(344, 152)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QtCore.QSize(344, 152))
+        MainWindow.setMaximumSize(QtCore.QSize(344, 152))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 70, 131, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 10, 201, 16))
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setEnabled(False)
+        self.lineEdit.setGeometry(QtCore.QRect(10, 30, 281, 23))
+        self.lineEdit.setObjectName("lineEdit")
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setGeometry(QtCore.QRect(10, 110, 321, 23))
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setObjectName("progressBar")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(300, 30, 31, 23))
+        self.pushButton_2.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../../Downloads/folder.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("../../Downloads/folder.png"), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.pushButton_2.setIcon(icon)
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Separador de Imagens"))
+        self.pushButton.setText(_translate("MainWindow", "Processar Imagens"))
+        self.label.setText(_translate("MainWindow", "Diretorio das imagens:"))
+
+
